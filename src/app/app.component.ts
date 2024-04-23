18ff4ed8186b91e9196bedcffb94666e2ccd0650
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2024-04-23 13:16:43 -0400

    Render user form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
+import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { AddressComponent } from './address/address.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @Component({
-  selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [ProfileComponent, AddressComponent, ReactiveFormsModule, JsonPipe],
 })
 export class AppComponent {
-  title = 'control-value-accesor';
+  public userForm = new FormGroup({
+    profile: new FormControl({
+      firstName: 'Richard',
+      lastName: 'Zhunio',
+    }),
+    address: new FormControl({
+      street: '20 Oxford Rd',
+      city: 'New Windsor',
+      state: 'NY',
+      zipCode: '12553',
+      country: 'United States',
+    }),
+  });
 }
